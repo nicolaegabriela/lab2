3b7587d993c3c00892afc604f6119ccde8716a50
ios2 <ios2@serbans-mbp-2.ad.qualitance.com>
2015-07-07 11:11:10 +0300

    Initial Commit

---
+++ Places/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Places
+//
+//  Created by ios2 on 07/07/15.
+//  Copyright (c) 2015 Gabriela. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Places/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Places
+//
+//  Created by ios2 on 07/07/15.
+//  Copyright (c) 2015 Gabriela. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
