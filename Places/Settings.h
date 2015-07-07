571aa993e09fdd70810f871382ed10fb0f7b27e1
ios2 <ios2@serbans-mbp-2.ad.qualitance.com>
2015-07-07 17:08:53 +0300

    lab2 finish

---
+++ Places/Settings.h
@@ -0,0 +1,13 @@
+//
+//  Settings.h
+//  Places
+//
+//  Created by ios2 on 07/07/15.
+//  Copyright (c) 2015 Gabriela. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Settings : NSObject
+
+@end
